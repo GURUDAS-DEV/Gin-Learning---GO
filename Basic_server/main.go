25b0f50bbf7871fedfc7adaaa967cf0571d3c9ef
Gurudas Bhardwaj <gursad5@gmail.com>
2026-09-25 12:00:00 +0530

    25

--- Basic_server/main.go
+++ Basic_server/main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5500", "http://localhost:8080"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * 60 * 60,
+	}))
+
 	r.GET("Ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Pong",
 		})
+	})
+
+	r.GET("/redirect", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusMovedPermanently, "https://gurudes.tech")
 	})
 
 	r.Run()
